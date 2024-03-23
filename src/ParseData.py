c66e54228aa1450434433442f5ed8d075988c542
Cara Stievater <cgs9212@rit.edu>
2024-03-23 08:47:41 -0400

    dataclass more or less updated (but not complete)

--- src/ParseData.py
+++ src/ParseData.py
@@ -4,6 +4,8 @@
     Parse through Data to get each individual section
 """
 
+import dataclasses as dataclass
+
 file01 = "checkpoints_eoc.csv"
 file02 = "checkpoints_pulse.csv"
 file03 = "items.csv"
@@ -11,6 +13,26 @@ file04 = "media_views.csv"  #Student ID
 file05 = "page_views.csv"   #Student ID needed
 file06 = "responses.csv"
 
+@dataclass
+class Student:
+    engaged : list # active time -> page_views.csv
+    intitute_id : int # page_views.csv
+    off_page_breif : list # (filter through for 45 minutes) page_views.csv
+    tried_again_clicks : list # page_views.csv
+    was_complete : list # page_views.csv
+    attempt : list # responses.csv
+    dt_submitted : list # responses.csv
+    item_type : list # responses.csv
+    lrn_dt_started : list # responses.csv
+    lrn_dt_saved : list # responses.csv
+    page : list # responses.csv
+    points_earned : list # responses.csv
+    points_possible : list # responses.csv
+    release_version : int # responses.csv
+
 #test01 to give
 def main():
+
+    # while going through the data we 
+
     pass
