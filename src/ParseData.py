5f0a29b3ce06e16f166f4d964c793967d497ecb6
Vivian Hernandex <vah7365@rit.edu>
2024-03-22 22:28:30 -0400

    Vivian: ParseData initial

--- src/ParseData.py
+++ src/ParseData.py
@@ -1,2 +1,16 @@
+"""
+    file: ParseData.py
+    description:
+    Parse through Data to get each individual section
+"""
+
+file01 = "checkpoints_eoc.csv"
+file02 = "checkpoints_pulse.csv"
+file03 = "items.csv"
+file04 = "media_views.csv"  #Student ID
+file05 = "page_views.csv"   #Student ID needed
+file06 = "responses.csv"
+
+#test01 to give
 def main():
     pass
