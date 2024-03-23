923453614183407e63a301c5c0b80c8429cc4ca7
Cara Stievater <cgs9212@rit.edu>
2024-03-23 15:00:08 -0400

    Updates to path setup

--- src/ParseData.py
+++ src/ParseData.py
@@ -7,13 +7,46 @@ from dataclasses import dataclass
 import csv
 import pandas as pd
 
-file01 = "checkpoints_eoc.csv"
-file02 = "checkpoints_pulse.csv"
-file03 = "items.csv"
-file04 = "media_views.csv"  #Student ID
-file05 = "page_views.csv"   #Student ID needed
-file06 = "responses.csv"
+file01 = "../datafest_csv_data/checkpoints_eoc.csv"
+file02 = "../datafest_csv_data/checkpoints_pulse.csv"
+file03 = "../datafest_csv_data/items.csv"
+file04 = "../datafest_csv_data//media_views.csv"  #Student ID
+file05 = ".../datafest_csv_data/page_views.csv"   #Student ID needed
+#file06 = "../datafest_csv_data/responses.csv"
 
-#test01 to give
+@dataclass
+class Student:
+    engaged : list # active time -> page_views.csv
+    # intitute_id : int # page_views.csv
+    # off_page_breif : list # (filter through for 45 minutes) page_views.csv
+    # tried_again_clicks : list # page_views.csv
+    # was_complete : list # page_views.csv
+    # attempt : list # responses.csv
+    # dt_submitted : list # responses.csv
+    # item_type : list # responses.csv
+    # lrn_dt_started : list # responses.csv
+    # lrn_dt_saved : list # responses.csv
+    # page : list # responses.csv
+    # points_earned : list # responses.csv
+    # points_possible : list # responses.csv
+    # release_version : int # responses.csv
+        
+"""
+Reads the data from the pageviews 
+:param 
+:return
+"""
+
+def read_pageviews(CSVfile):
+    read = pd.read_csv(CSVfile, sep=',')
+
 def main():
-    pass
+    read_pageviews(file05)
+    
+    students = dict
+    chunk_size = 1
+
+    for chunk in pd.read_csv(file01, chunksize=chunk_size): 
+        print(chunk)
+
+main()
